7f447f479a5266cbe4b15ad1cc3cdead0e168dce
ThiagoPuyelli <thiagopuyelli@gmail.com>
2025-04-08 17:03:58 -0300

    index

---
+++ js/header.js
@@ -0,0 +1,24 @@
+const navIcons = document.querySelector(".nav-icons")
+const navLinks = document.querySelector(".nav-links")
+const burguer = document.createElement("button")
+const imgBurguer = document.createElement("img")
+imgBurguer.setAttribute("src", "img/menu.png")
+imgBurguer.setAttribute("alt", "Menu")
+
+burguer.appendChild(imgBurguer)
+burguer.className = "menu-button"
+
+navIcons.appendChild(burguer)
+
+let openMenu = false
+
+burguer.addEventListener("click", function () {
+    if (!openMenu) {
+        burguer.style.transform = "rotateZ(90deg)"
+        navLinks.style.right = "0px"
+    } else {
+        burguer.style.transform = "rotateZ(0)"
+        navLinks.style.right = "100%"
+    }
+    openMenu = !openMenu
+})
